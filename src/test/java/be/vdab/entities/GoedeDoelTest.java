ce9a277739ff1169677c661f075c9b8564c9f5f0
kr0ma <karim.romagnani@gmail.com>
2015-12-23 08:51:56 +0100

    added failing test

--- src/test/java/be/vdab/entities/GoedeDoelTest.java
+++ src/test/java/be/vdab/entities/GoedeDoelTest.java
@@ -2,19 +2,27 @@ package be.vdab.entities;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigDecimal;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class GoedeDoelTest {
-	private static final String NAAM ="CliniClowns"; 
+	private static final String NAAM = "CliniClowns";
 	private GoedeDoel doel;
 
 	@Before
 	public void before() {
 		doel = new GoedeDoel(NAAM);
 	}
+
 	@Test
 	public void getNaam() {
 		assertEquals(NAAM, doel.getNaam());
+	}
+
+	@Test
+	public void eenNieuwDoelHeeftNogNietsOpgebracht() {
+		assertEquals(0, doel.getOpgebracht().compareTo(BigDecimal.ZERO));
 	}
 }
